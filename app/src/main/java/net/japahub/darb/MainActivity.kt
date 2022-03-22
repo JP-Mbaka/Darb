8e8aba497ec73b537bf133ee2ff4526edecb7c11
JP-Mbaka <mbakajohnpaul@gmail.com>
2022-03-22 12:59:11 +0100

    Bottom Navigation XML file design is complete and satisfactory, Remaining 1. Configuration of Bottom navigation to navigate to each file 2. Setup Classify Form 3. Setup Beta View 4. Add ML 5. Add Sparkql

--- app/src/main/java/net/japahub/darb/MainActivity.kt
+++ app/src/main/java/net/japahub/darb/MainActivity.kt
@@ -1,21 +1,79 @@
 package net.japahub.darb
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.widget.CompositePageTransformer
 import androidx.viewpager2.widget.MarginPageTransformer
 import androidx.viewpager2.widget.ViewPager2
-import java.lang.Math.abs
 
 private lateinit var viewPager2: ViewPager2
 private val sliderHandler = Handler()
+private lateinit var classify: ImageButton
+private lateinit var appointment: ImageButton
+private lateinit var reminder: ImageButton
+private lateinit var diet: ImageButton
+private lateinit var hospital: ImageButton
+private lateinit var contactDoctor: ImageButton
+private lateinit var settings: ImageButton
+private lateinit var help: ImageButton
+private lateinit var emergency: ImageButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        classify = findViewById(R.id.classify)
+        appointment = findViewById(R.id.appointment)
+        reminder = findViewById(R.id.reminder)
+        diet = findViewById(R.id.diet)
+        hospital = findViewById(R.id.hospital)
+        contactDoctor = findViewById(R.id.contact_doctor)
+        settings = findViewById(R.id.settings)
+        help = findViewById(R.id.help)
+        emergency = findViewById(R.id.emergency)
+
+        classify.setOnClickListener{
+            val intent = Intent(this, Classify::class.java)
+            startActivity(intent)
+        }
+        appointment.setOnClickListener{
+            val intent = Intent(this, Hospital::class.java)
+            startActivity(intent)
+        }
+        reminder.setOnClickListener{
+            val intent = Intent(this, BetaView::class.java)
+            startActivity(intent)
+        }
+        diet.setOnClickListener{
+            val intent = Intent(this, BetaView::class.java)
+            startActivity(intent)
+        }
+        hospital.setOnClickListener{
+            val intent = Intent(this, Hospital::class.java)
+            startActivity(intent)
+        }
+        contactDoctor.setOnClickListener{
+            val intent = Intent(this, BetaView::class.java)
+            startActivity(intent)
+        }
+        settings.setOnClickListener{
+            val intent = Intent(this, BetaView::class.java)
+            startActivity(intent)
+        }
+        help.setOnClickListener{
+            val intent = Intent(this, Help::class.java)
+            startActivity(intent)
+        }
+        emergency.setOnClickListener{
+            val intent = Intent(this, Emergency::class.java)
+            startActivity(intent)
+        }
+
 
         viewPager2 = findViewById(R.id.viewPager_ImageSlider)
 
@@ -34,7 +92,7 @@ class MainActivity : AppCompatActivity() {
         val compositePageTransformer = CompositePageTransformer()
         compositePageTransformer.addTransformer(MarginPageTransformer(30))
         compositePageTransformer.addTransformer{ page, position ->
-            val r = 1 - abs(position)
+            val r = 1 - kotlin.math.abs(position)
             page.scaleY = 0.85f + r * 0.25f
         }
         viewPager2.setPageTransformer(compositePageTransformer)
@@ -43,7 +101,7 @@ class MainActivity : AppCompatActivity() {
             override fun onPageSelected(position: Int) {
                 super.onPageSelected(position)
                 sliderHandler.removeCallbacks(sliderRunnable)
-                sliderHandler.postDelayed(sliderRunnable, 3000,)
+                sliderHandler.postDelayed(sliderRunnable, 3000)
             }
         })
     }
